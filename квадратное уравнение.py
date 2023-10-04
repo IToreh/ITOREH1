10dad049943c37cc344e9dca089de56f5530e06b
IToreh <135696571+IToreh@users.noreply.github.com>
2023-10-04 12:58:03 +0300

    Add files via upload

---
+++ квадратное уравнение.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = b ** 2 - 4 * a * c
+if d > 0:
+    x1 = (-b + d**0.5)/2*a
+    x2 = (-b - d**0.5)/2*a
+    print(round(x1, 2), round(x2, 2) )
+elif d == 0:
+    x = -b/2*a
+    print(round(x, 2))
+else:
+    print("нет корней")
